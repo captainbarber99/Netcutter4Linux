f108949f54ad827f0bbc07b4bd5c693e875b0257
M0rph3u5 <divyanshushukl@gmail.com>
2017-01-25 18:33:00 +0530

    Update geoip.sh

--- geoip.sh
+++ geoip.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
 
-cat /home/morpheus/iplist.txt | awk '{print $Z}' > ipformat.txt
-ipformat.txt|uniq > mainiplist.txt
-IP=`cat mainiplist.txt`
-for i in $IP
-do
-  echo "$i,`geoiplookup $i >> info.txt
-done
